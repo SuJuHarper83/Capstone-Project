fb94ddcd599af514eb91ee6cba7ab99eabf97160
Your Name <you@example.com>
2023-01-16 11:51:55 -0700

    Corrected package installations; Created urls and serializers files

---
+++ backend/capstone/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from rest_framework.response import Response
+from rest_framework import status
+
+# Create your views here.
+
+
